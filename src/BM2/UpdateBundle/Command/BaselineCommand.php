ff48bf5a508ab77854ffd1ac33d5130fb3882ee6
Andrew <Andrew@SeventhGamers.com>
2016-10-05 22:47:51 +0900

    Create BaselineCommand.php
    A shell right now. When finished, will be used to establish initial baseline of where the game was when it started/this was implemented, and again after any major changes (like to table columns or entire tables) are applied.

---
+++ src/BM2/UpdateBundle/Command/BaselineCommand.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BM2\UpdateBundle\Command;
+
+use BM2\UpdateBundle\Entity\Baseline;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BaselineCommand extends ContainerAwareCommand {
+
+}
