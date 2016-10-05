69dc9ffdf0110ae656ca75610c158b6a35d88dac
Andrew <Andrew@SeventhGamers.com>
2016-10-05 23:16:19 +0900

    Update and rename UpdateCommand.php to TweakCommand.php
    I might change the name again but something about having to type update:update in the future sounded annoying.

--- src/BM2/UpdateBundle/Command/TweakCommand.php
+++ src/BM2/UpdateBundle/Command/TweakCommand.php
@@ -2,7 +2,6 @@
 
 namespace BM2\UpdateBundle\Command;
 
-use BM2\UpdateBundle\Entity\Update;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -10,5 +9,12 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class UpdateCommand extends ContainerAwareCommand {
+	
+	protected function configure() {
+		$this
+			->setName('update:tweak')
+			->setDescription('Use to apply minor changes to the database, like adding or removing an item or changing its values.')
+		;
+	}
 
 }
