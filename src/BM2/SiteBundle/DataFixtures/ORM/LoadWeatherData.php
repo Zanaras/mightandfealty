176bed109f919b958f609001e2a73cc707ec3fe6
Andrew <Andrew@SeventhGamers.com>
2016-11-20 02:05:51 +0900

    Update LoadWeatherData.php

--- src/BM2/SiteBundle/DataFixtures/ORM/LoadWeatherData.php
+++ src/BM2/SiteBundle/DataFixtures/ORM/LoadWeatherData.php
@@ -9,7 +9,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use BM2\SiteBundle\Entity\WeatherType;
 
 class LoadWeatherData extends AbstractFixture implements OrderedFixtureInterface {
-	private $entourage = array(
+	
+	private $weather = array(
 		'no wind'		=> array('type' =>       'wind', 'wind' =>   1, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'light breeze'		=> array('type' =>       'wind', 'wind' => 0.9, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'mild breeze'		=> array('type' =>       'wind', 'wind' => 0.8, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
@@ -17,24 +18,29 @@ class LoadWeatherData extends AbstractFixture implements OrderedFixtureInterface
 		'medium wind'		=> array('type' =>       'wind', 'wind' => 0.5, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'heavy wind'		=> array('type' =>       'wind', 'wind' => 0.3, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'gale force wind'	=> array('type' =>       'wind', 'wind' =>0.05, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
-		'no rain'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
-		'drizzle'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>0.97, 'precipitation' => 0.9, 'visibility' =>0.95, 'magic' =>   1),
-		'light rain'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>0.93, 'precipitation' =>0.75, 'visibility' => 0.9, 'magic' =>   1),
-		'rain'			=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.9, 'precipitation' => 0.6, 'visibility' => 0.8, 'magic' =>   1),
-		'heavy rain'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.8, 'precipitation' => 0.4, 'visibility' => 0.7, 'magic' =>   1),
-		'torrential rain'	=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1),
+		'no weather'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
+		'drizzle'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>0.97, 'precipitation' => 0.9, 'visibility' =>0.95, 'magic' =>   1, 'mintemp' => 32),
+		'light rain'		=> array('type' =>    'weather', 'wind' =>   1, 'light' =>0.93, 'precipitation' =>0.75, 'visibility' => 0.9, 'magic' =>   1, 'mintemp' => 32),
+		'rain'			=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.9, 'precipitation' => 0.6, 'visibility' => 0.8, 'magic' =>   1, 'mintemp' => 32),
+		'heavy rain'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.8, 'precipitation' => 0.4, 'visibility' => 0.7, 'magic' =>   1, 'mintemp' => 32),
+		'torrential rain'	=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'mintemp' => 32),
+		'rain snow mix'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'maxtemp' => 42),
+		'light snow'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'maxtemp' => 42),
+		'snow'			=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'maxtemp' => 42),
+		'heavy snow'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'maxtemp' => 42),
+		'blizzard'		=> array('type' =>    'weather', 'wind' =>   1, 'light' => 0.7, 'precipitation' => 0.1, 'visibility' =>   1, 'magic' =>   1, 'maxtemp' => 42),
 		'clear sky'		=> array('type' =>        'sky', 'wind' =>   1, 'light' =>   1, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'partly cloudy'		=> array('type' =>        'sky', 'wind' =>   1, 'light' =>0.95, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'cloudy'		=> array('type' =>        'sky', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'heavy cloudcover'	=> array('type' =>        'sky', 'wind' =>   1, 'light' => 0.8, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
-		'no fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
-		'light fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' =>0.95, 'precipitation' =>   1, 'visibility' => 0.9, 'magic' =>   1),
-		'fog'			=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' => 0.7, 'magic' =>   1),
-		'heavy fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.8, 'precipitation' =>   1, 'visibility' => 0.5, 'magic' =>   1),
-		'thick fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.7, 'precipitation' =>   1, 'visibility' => 0.4, 'magic' =>   1),
-		'light dust storm'	=> array('type' => 'visibility', 'wind' =>   1, 'light' =>0.95, 'precipitation' =>   1, 'visibility' =>0.85, 'magic' =>   1),
-		'dust storm'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' => 0.7, 'magic' =>   1),
-		'heavy dust storm'	=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.8, 'precipitation' =>   1, 'visibility' => 0.5, 'magic' =>   1),
+		'no fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1, 'mintemp' => 32),
+		'light fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' =>0.95, 'precipitation' =>   1, 'visibility' => 0.9, 'magic' =>   1, 'mintemp' => 32),
+		'fog'			=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' => 0.7, 'magic' =>   1, 'mintemp' => 32),
+		'heavy fog'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.8, 'precipitation' =>   1, 'visibility' => 0.5, 'magic' =>   1, 'mintemp' => 32),
+		'very heavy fog'	=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.7, 'precipitation' =>   1, 'visibility' => 0.4, 'magic' =>   1, 'mintemp' => 32),
+		'light dust storm'	=> array('type' => 'visibility', 'wind' =>   1, 'light' =>0.95, 'precipitation' =>   1, 'visibility' =>0.85, 'magic' =>   1, 'biome' => array('badlands', 'desert')),
+		'dust storm'		=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.9, 'precipitation' =>   1, 'visibility' => 0.7, 'magic' =>   1, 'biome' => array('badlands', 'desert')),
+		'heavy dust storm'	=> array('type' => 'visibility', 'wind' =>   1, 'light' => 0.8, 'precipitation' =>   1, 'visibility' => 0.5, 'magic' =>   1, 'biome' => array('badlands', 'desert')),
 		'new moon'		=> array('type' =>      'lunar', 'wind' =>   1, 'light' => 0.5, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'quarter moon'		=> array('type' =>      'lunar', 'wind' =>   1, 'light' =>0.63, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
 		'half moon'		=> array('type' =>      'lunar', 'wind' =>   1, 'light' =>0.75, 'precipitation' =>   1, 'visibility' =>   1, 'magic' =>   1),
@@ -52,13 +58,13 @@ class LoadWeatherData extends AbstractFixture implements OrderedFixtureInterface
 	 * {@inheritDoc}
 	 */
 	public function getOrder() {
-		return 15; // requires LoadBuildingData.php
+		return 15; // Requires LoadBiomeData
 	}
 	/**
 	 * {@inheritDoc}
 	 */
 	public function load(ObjectManager $manager) {
-		foreach ($this->biomes as $name=>$data) {
+		foreach ($this->weather as $name=>$data) {
 			$type = new Weather;
 			$type->setName($name);
 			$type->setType($data['type']);
@@ -67,6 +73,9 @@ class LoadWeatherData extends AbstractFixture implements OrderedFixtureInterface
 			$type->setPrecipitation($data['precipitation']);
 			$type->setVisibility($data['visibility']);
 			$type->setMagic($data['magic']);
+			$type->setMinTemp(isset($data['mintemp'])?$data['mintemp']:NULL);
+			$type->setMaxTemp(isset($data['maxtemp'])?$data['maxtemp']:NULL);
+			$type->setBiome(isset($data['biome'])?$data['biome']:NULL);
 			$manager->persist($type);
 		}
 		$manager->flush();
